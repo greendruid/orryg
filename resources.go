77ad21f8e0030c47824122634895e7b7a532471c
Vincent Rischmann <me@vrischmann.me>
2015-12-05 01:27:27 +0100

    run the installed binary bin2hex

--- resources.go
+++ resources.go
@@ -1,5 +1,3 @@
 package main
 
-// Add a new comment line each time you add a resource.
-
-//go:generate go run cmd/bin2hex/main.go main cancelPNG=cancel.png
+//go:generate bin2hex main cancelPNG=cancel.png
